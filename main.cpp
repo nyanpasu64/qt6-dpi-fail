05ecf89f9cbbe2e78e159bc5ba8ff9fafcd157d2
nyanpasu64 <nyanpasu64@tuta.io>
2021-08-25 10:42:16 -0700

    QProxyStyle crash

--- main.cpp
+++ main.cpp
@@ -1,41 +1,38 @@
-#include <QApplication>
+#include <QPushButton>
 #include <QWidget>
 
 #include <QVBoxLayout>
-#include <QPushButton>
-#include <QDebug>
-#include <QMetaEnum>
 
-template<typename EnumT>
-QString stringify(const EnumT value)
-{
-  return QMetaEnum::fromType<EnumT>().valueToKey((int) value);
-}
+#include <QApplication>
+#include <QDebug>
+#include <QProxyStyle>
 
 int main(int argc, char *argv[])
 {
-    QGuiApplication::setHighDpiScaleFactorRoundingPolicy(
-        Qt::HighDpiScaleFactorRoundingPolicy::RoundPreferFloor
-    );
-    qDebug() << QGuiApplication::highDpiScaleFactorRoundingPolicy();
     QApplication a(argc, argv);
     QWidget c;
 
     auto l = new QVBoxLayout;
     c.setLayout(l);
-    l->setSizeConstraint(QLayout::SetFixedSize);
 
-    auto w = new QPushButton("Query devicePixelRatio()");
-    l->addWidget(w);
-    w->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+    {
+        auto w = new QPushButton("QProxyStyle(QApplication::style())");
+        l->addWidget(w);
+
+        QObject::connect(w, &QPushButton::pressed, w, [w]() {
+            (QProxyStyle(QApplication::style()));
+        });
+    }
+
+    {
+        auto w = new QPushButton("QProxyStyle(w->style())");
+        l->addWidget(w);
+
+        QObject::connect(w, &QPushButton::pressed, w, [w]() {
+            QProxyStyle(w->style());
+        });
+    }
+
     c.show();
-
-    QObject::connect(w, &QPushButton::pressed, w, [w]() {
-        w->setText(QString("%1 -> %2")
-            .arg(stringify(QGuiApplication::highDpiScaleFactorRoundingPolicy()))
-            .arg(w->devicePixelRatio())
-        );
-    });
-
     return a.exec();
 }
