1b8caa3fe010a9b5ef5ee7a24dad5302a8aee1fd
nyanpasu64 <nyanpasu64@tuta.io>
2021-09-02 19:07:28 -0700

    Find which widget was clicked

--- main.cpp
+++ main.cpp
@@ -1,36 +1,53 @@
-#include <QPushButton>
 #include <QWidget>
 
 #include <QVBoxLayout>
 
 #include <QApplication>
 #include <QDebug>
-#include <QProxyStyle>
+#include <QListWidget>
+#include <QMouseEvent>
+
+class FindWidgetClicked : public QObject
+{
+protected:
+    bool eventFilter(QObject *obj, QEvent *event) override;
+};
+
+bool FindWidgetClicked::eventFilter(QObject *obj, QEvent *event)
+{
+    auto w = qobject_cast<QWidget *>(obj);
+    if (!w) throw "up";
+
+    if (event->type() == QEvent::MouseButtonPress) {
+        QMouseEvent *e = static_cast<QMouseEvent *>(event);
+        auto p = QPoint(e->x(), e->y());
+
+        while (auto child = w->childAt(p)) {
+            w = child;
+            p -= w->geometry().topLeft();
+        }
+
+        qDebug() << w;
+        return true;
+    } else {
+        return QObject::eventFilter(obj, event);
+    }
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QWidget c;
 
+    FindWidgetClicked filter;
+    c.installEventFilter(&filter);
+
     auto l = new QVBoxLayout;
     c.setLayout(l);
 
     {
-        auto w = new QPushButton("QProxyStyle(QApplication::style())");
+        auto w = new QListWidget;
         l->addWidget(w);
-
-        QObject::connect(w, &QPushButton::pressed, w, [w]() {
-            (QProxyStyle(QApplication::style()));
-        });
-    }
-
-    {
-        auto w = new QPushButton("QProxyStyle(w->style())");
-        l->addWidget(w);
-
-        QObject::connect(w, &QPushButton::pressed, w, [w]() {
-            QProxyStyle(w->style());
-        });
     }
 
     c.show();
