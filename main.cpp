0602ded1a0ea468acecd73827439cefe2820905b
nyanpasu64 <nyanpasu64@tuta.io>
2021-09-06 03:43:48 -0700

    QSpinBox::setFocusPolicy(Qt::StrongFocus)

--- main.cpp
+++ main.cpp
@@ -1,57 +1,33 @@
 #include <QWidget>
 
-#include <QListWidget>
-#include <QPushButton>
+#include <QLineEdit>
+#include <QSpinBox>
 #include <QVBoxLayout>
 
 #include <QApplication>
 #include <QDebug>
-#include <QMouseEvent>
 
-class FindWidgetClicked : public QObject
-{
-protected:
-    bool eventFilter(QObject *obj, QEvent *event) override;
-};
-
-bool FindWidgetClicked::eventFilter(QObject *obj, QEvent *event)
-{
-    auto w = qobject_cast<QWidget *>(obj);
-    if (!w) throw "up";
-
-    if (event->type() == QEvent::MouseButtonPress) {
-        QMouseEvent *e = static_cast<QMouseEvent *>(event);
-        auto p = e->pos();
-
-        while (auto child = w->childAt(p)) {
-            w = child;
-            p -= w->geometry().topLeft();
-        }
-
-        qDebug() << w;
-        return true;
-    } else {
-        return QObject::eventFilter(obj, event);
-    }
-}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QWidget c;
 
-    FindWidgetClicked filter;
-    c.installEventFilter(&filter);
-
     auto l = new QVBoxLayout;
     c.setLayout(l);
 
     {
-        auto w = new QListWidget;
+        auto w = new QSpinBox;
+        w->setFocusPolicy(Qt::StrongFocus);
         l->addWidget(w);
     }
     {
-        auto w = new QPushButton;
+        auto w = new QSpinBox;
+        w->setFocusPolicy(Qt::StrongFocus);
+        l->addWidget(w);
+    }
+    {
+        auto w = new QLineEdit;
         l->addWidget(w);
     }
 
